0519d8e8fd99e6d3abe3eb7b6269e74f44b1ff7f
ismael_br7@hotmail.com <ismael_br7@hotmail.com>
2014-12-09 20:58:15 -0600

    Paso 4
    Evento Click para deslizar Sliders

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(document).on('ready',function() {
+	init();
+});
 
+function init(){
+	$('.slide_controls li').on('click', handleClick);
+	var width = $('.slider_container').width();
+	$('.slide').each(function(i,e){
+		$(e).css('width',width+'px'); 
+	});
+}
+
+function handleClick(){
+	var slide_target = 0;
+	if($(this).parent().hasClass('slide_controls')){
+		slide_target =$(this).index();
+	}
+
+	$('.sliderContainer').fadeOut('slow',function(){
+		$(this).animate({
+			'margin-left' :-(slide_target * $('.slider_container').width())+'px'
+		}, 'slow',function(){
+			$(this).fadeIn();
+		});
+	});
+}
